db3a14192b48acaa2ae3542837e8a529058272c7
jenniffermuima. <jenniffermuima2001@gmail.com>
2024-10-30 15:39:14 -0500

    Jenniffer

---
+++ src/main/java/ec/edu/espoch/programa00/Programa00.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.programa00;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Programa00 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
